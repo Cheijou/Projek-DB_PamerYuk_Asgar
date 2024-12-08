23f2a0421a5ce324d20f8f1ed173f88f6676600f
Nornyr <ignatiusfelixc@gmail.com>
2024-12-08 22:55:18 +0700

    tambah form profile + ubah

---
+++ PamerYuk_Asgar/PamerYuk_Asgar/FormEditProfil.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace PamerYuk_Asgar
+{
+    partial class FormEditProfil
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonSimpan = new System.Windows.Forms.Button();
+            this.buttonTutup = new System.Windows.Forms.Button();
+            this.textBoxUsername = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.textBoxTanggalLahir = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.buttonUbah = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.textBoxKota = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(4, -1);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(438, 78);
+            this.panel1.TabIndex = 34;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(93, 10);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(249, 52);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ubah Profil";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // buttonSimpan
+            // 
+            this.buttonSimpan.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.buttonSimpan.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSimpan.Location = new System.Drawing.Point(324, 390);
+            this.buttonSimpan.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonSimpan.Name = "buttonSimpan";
+            this.buttonSimpan.Size = new System.Drawing.Size(100, 28);
+            this.buttonSimpan.TabIndex = 40;
+            this.buttonSimpan.Text = "Simpan";
+            this.buttonSimpan.UseVisualStyleBackColor = false;
+            // 
+            // buttonTutup
+            // 
+            this.buttonTutup.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.buttonTutup.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonTutup.Location = new System.Drawing.Point(13, 390);
+            this.buttonTutup.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonTutup.Name = "buttonTutup";
+            this.buttonTutup.Size = new System.Drawing.Size(100, 28);
+            this.buttonTutup.TabIndex = 39;
+            this.buttonTutup.Text = "Tutup";
+            this.buttonTutup.UseVisualStyleBackColor = false;
+            // 
+            // textBoxUsername
+            // 
+            this.textBoxUsername.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.textBoxUsername.Location = new System.Drawing.Point(98, 11);
+            this.textBoxUsername.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxUsername.Name = "textBoxUsername";
+            this.textBoxUsername.Size = new System.Drawing.Size(294, 22);
+            this.textBoxUsername.TabIndex = 37;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 14);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 16);
+            this.label2.TabIndex = 35;
+            this.label2.Text = "Username :";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.panel2.Controls.Add(this.textBoxUsername);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(12, 220);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(411, 43);
+            this.panel2.TabIndex = 44;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.panel3.Controls.Add(this.textBoxTanggalLahir);
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Location = new System.Drawing.Point(13, 269);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(411, 43);
+            this.panel3.TabIndex = 45;
+            // 
+            // textBoxTanggalLahir
+            // 
+            this.textBoxTanggalLahir.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.textBoxTanggalLahir.Location = new System.Drawing.Point(98, 11);
+            this.textBoxTanggalLahir.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxTanggalLahir.Name = "textBoxTanggalLahir";
+            this.textBoxTanggalLahir.Size = new System.Drawing.Size(294, 22);
+            this.textBoxTanggalLahir.TabIndex = 37;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(4, 14);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 16);
+            this.label3.TabIndex = 35;
+            this.label3.Text = "Tanggal Lahir :";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(162, 84);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(131, 92);
+            this.pictureBox1.TabIndex = 46;
+            this.pictureBox1.TabStop = false;
+            // 
+            // buttonUbah
+            // 
+            this.buttonUbah.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.buttonUbah.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonUbah.Location = new System.Drawing.Point(162, 183);
+            this.buttonUbah.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonUbah.Name = "buttonUbah";
+            this.buttonUbah.Size = new System.Drawing.Size(131, 28);
+            this.buttonUbah.TabIndex = 47;
+            this.buttonUbah.Text = "Ubah Foto";
+            this.buttonUbah.UseVisualStyleBackColor = false;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.panel4.Controls.Add(this.textBoxKota);
+            this.panel4.Controls.Add(this.label4);
+            this.panel4.Location = new System.Drawing.Point(13, 318);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(411, 43);
+            this.panel4.TabIndex = 46;
+            // 
+            // textBoxKota
+            // 
+            this.textBoxKota.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.textBoxKota.Location = new System.Drawing.Point(98, 11);
+            this.textBoxKota.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxKota.Name = "textBoxKota";
+            this.textBoxKota.Size = new System.Drawing.Size(294, 22);
+            this.textBoxKota.TabIndex = 37;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(4, 14);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 16);
+            this.label4.TabIndex = 35;
+            this.label4.Text = "Kota :";
+            // 
+            // FormEditProfil
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(435, 440);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.buttonUbah);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.buttonSimpan);
+            this.Controls.Add(this.buttonTutup);
+            this.Name = "FormEditProfil";
+            this.Text = "FormEditProfil";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonSimpan;
+        private System.Windows.Forms.Button buttonTutup;
+        private System.Windows.Forms.TextBox textBoxUsername;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox textBoxTanggalLahir;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button buttonUbah;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.TextBox textBoxKota;
+        private System.Windows.Forms.Label label4;
+    }
+}
